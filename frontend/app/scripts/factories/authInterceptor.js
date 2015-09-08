7217cd4e2d4af16b2a44a179173f2dfb9e19d1d6
tester <test@test.de>
2015-09-08 14:56:22 +0200

    better error handling

--- frontend/app/scripts/factories/authInterceptor.js
+++ frontend/app/scripts/factories/authInterceptor.js
@@ -1,7 +1,10 @@
 (function () {
     'use strict';
 
-    angular.module('foodCircle').factory('authInterceptor', ['authToken', function (authToken) {
+    angular.module('foodCircle').factory('authInterceptor', ['authToken', 'AUTH_EVENTS', '$rootScope', '$q', function (authToken, AUTH_EVENTS, $rootScope, $q) {
+
+
+
         return {
             request: function (config) {
                 var token = authToken.getToken();
@@ -14,6 +17,17 @@
             },
             response: function (response) {
                 return response;
+            },
+
+            responseError: function (response) {
+                $rootScope.$broadcast({
+                    401: AUTH_EVENTS.notAuthenticated,
+                    403: AUTH_EVENTS.notAuthorized,
+                    419: AUTH_EVENTS.sessionTimeout,
+                    440: AUTH_EVENTS.sessionTimeout
+                }[response.status], response);
+
+                return $q.reject(response);
             }
         };
     }]);
