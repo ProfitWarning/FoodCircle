65d3fcd5b76189221e63a01879df159e61e14cba
ProfitWarning <christian.fastner@arcor.de>
2015-10-25 16:08:05 +0100

    translated edit reipe,  navigation

--- frontend/app/scripts/app.config.translate.js
+++ frontend/app/scripts/app.config.translate.js
@@ -9,7 +9,7 @@
     angular.module('config.translate', ['pascalprecht.translate']).config(['$translateProvider', function ($translateProvider) {
 
         $translateProvider.preferredLanguage('de');
-        $translateProvider.useSanitizeValueStrategy('sanitize');
+        $translateProvider.useSanitizeValueStrategy('sanitizeParameters');
 
         $translateProvider.translations('de', {
             nav: {
@@ -31,8 +31,49 @@
             },
             login: {
                 headline: 'Sign in'
-            }
+            },
+            recipe: {
+                name: '',
+                description: 'Zubereitung',
+                ingrediants: 'Zutaten',
+                units: {
+                    g: 'g',
+                    kg: 'kg',
+                    litre: 'Liter',
+                    pinch: 'Priese(n)',
+                    small: 'kleine',
+                    big: 'große',
+                    tin: 'Dose',
+                    squirt: 'Spritzer'
+                }
 
+            },
+            edit: {
+                action: {
+                    save: ' Speichern',
+                    editimage: ' Bilder bearbeiten',
+                    addingrediant: ' hinzufügen'
+                },
+                label: {
+                    name: 'Name',
+                    description: 'Zubereitung',
+                    ingredients: 'Zutaten'
+                },
+                placeholder: {
+                    name: 'Name',
+                    description: 'Zubereitung',
+                    amount: 'Menge',
+                    ingredient: 'Zutat'
+                },
+                validation: {
+                    required: {
+                        amount: 'Menge wird benötigt',
+                        name: 'Name wird benötigt',
+                        description: 'Zubereitung wird benötigt',
+                        unit: 'Einheit wird benötigt'
+                    }
+                }
+            }
         });
 
     }]);
